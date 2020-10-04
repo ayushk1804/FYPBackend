854113b7d3edfe08de872a4dec06d73b7db48420
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2020-10-04 12:37:20 +0530

    initialised

---
+++ src/Application.kt
@@ -0,0 +1,51 @@
+package com.vishwa.demo
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+
+fun main()
+{
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world Vishwa!", ContentType.Text.Plain)
+            }
+        }
+    }
+    server.start(wait = false)
+}
+
+//fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+//@Suppress("unused") // Referenced in application.conf
+//@kotlin.jvm.JvmOverloads
+//fun Application.module(testing: Boolean = false) {
+//    val server = embeddedServer(Netty, 8080) {
+//        routing {
+//            get("/") {
+//                call.respondText("Hello, world!", ContentType.Text.Html)
+//            }
+//        }
+//    }
+//    server.start(wait = true)
+//}
+
+//fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
+//    style(type = ContentType.Text.CSS.toString()) {
+//        +CSSBuilder().apply(builder).toString()
+//    }
+//}
+//
+//fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
+//    this.style = CSSBuilder().apply(builder).toString().trim()
+//}
+//
+//suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+//    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+//}
