5e738b286bd7af0ab75eef2df0aa093b7804917f
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2020-10-07 19:10:56 +0530

    error fix

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -9,7 +9,6 @@ import io.ktor.html.*
 import kotlinx.html.*
 import kotlinx.css.*
 import io.ktor.client.*
-import io.ktor.client.engine.apache.*
 import kotlin.test.*
 import io.ktor.server.testing.*
 
