cb35632dc0047129a7ded41ac8fc8bbef30b03ca
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2020-10-08 15:24:17 +0530

    u#16

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ apt-get install -y gnupg
 apt-get install -y wget
 apt-get install -y sudo
 apt-get install -y systemctl
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
 sudo systemctl start mongod
