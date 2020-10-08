350da13ab6607cd45eeebe326cc4a1749cb90c3d
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2020-10-08 14:51:43 +0530

    u#11

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 apt-get update
+echo "Asia/Kolkata" | tee /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
 apt-get install -y openjdk-8-jdk
 java -server -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:InitialRAMFraction=2 -XX:MinRAMFraction=2 -XX:MaxRAMFraction=2 -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+UseStringDeduplication -jar demo.jar
