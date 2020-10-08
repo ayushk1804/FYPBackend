2c84078e9c4a793695807326e19f79fe0ce3ef1c
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2020-10-08 14:36:49 +0530

    u#9

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-echo "Vishwa"
+apt-get install openjdk-8-jre
+java -server -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:InitialRAMFraction=2 -XX:MinRAMFraction=2 -XX:MaxRAMFraction=2 -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+UseStringDeduplication -jar demo.jar
