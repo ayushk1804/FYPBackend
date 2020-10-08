732699e642e19eec0cc5de41214094f5d9ca19aa
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2020-10-08 17:07:32 +0530

    u#20

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-sudo apt-get update
+apt-get update
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -y install tzdata
+apt-get -y install tzdata
 echo "Asia/Kolkata" | tee /etc/timezone
-sudo dpkg-reconfigure --frontend noninteractive tzdata
-sudo apt-get install -y openjdk-8-jdk
-sudo apt-get install -y gnupg
-sudo apt-get install -y wget
-sudo apt-get install -y sudo
-sudo apt-get install -y systemctl
-sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+#dpkg-reconfigure --frontend noninteractive tzdata
+apt-get install -y openjdk-8-jdk
+apt-get install -y gnupg
+apt-get install -y wget
+apt-get install -y sudo
+apt-get install -y systemctl
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
