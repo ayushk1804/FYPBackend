fac94870269b469ca90ceef95b3c9dd1fcb83b62
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2020-10-08 18:17:15 +0530

    u#22

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 #apt-get -y install tzdata
 #echo "Asia/Kolkata" | tee /etc/timezone
 #dpkg-reconfigure --frontend noninteractive tzdata
-apt-get install -y openjdk-8-jdk
+#apt-get install -y openjdk-8-jdk
 #apt-get install -y gnupg
 #apt-get install -y wget
 #apt-get install -y sudo
@@ -12,8 +12,8 @@ apt-get install -y openjdk-8-jdk
 #wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 #echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 #sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl daemon-reload
-sudo systemctl start mongod
-java -server -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:InitialRAMFraction=2 -XX:MinRAMFraction=2 -XX:MaxRAMFraction=2 -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+UseStringDeduplication -jar demo.jar
+#sudo apt-get install -y mongodb-org
+#sudo systemctl start mongod
+#sudo systemctl daemon-reload
+#sudo systemctl start mongod
+#java -server -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:InitialRAMFraction=2 -XX:MinRAMFraction=2 -XX:MaxRAMFraction=2 -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+UseStringDeduplication -jar demo.jar
