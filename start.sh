5a70a93e5ab9958e981695aea004eb2d2f678f10
ayushk780 <40500298+ayushk780@users.noreply.github.com>
2020-10-11 17:28:30 +0530

    Add cron startup script
    Systemctl start cron

--- start.sh
+++ start.sh
@@ -16,4 +16,6 @@
 #systemctl start mongod
 #sudo systemctl daemon-reload
 #sudo systemctl start mongod
+systemctl start cron && \
+mongod -bind_ip 0.0.0.0 & \
 java -server -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:InitialRAMFraction=2 -XX:MinRAMFraction=2 -XX:MaxRAMFraction=2 -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+UseStringDeduplication -jar demo.jar
